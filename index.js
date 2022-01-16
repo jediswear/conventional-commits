8f31140d0a1f9874d23dd759262e44a4864ae720
vzaiets <jediswear@gmail.com>
2022-01-16 19:13:59 +0200

    fix: fix for f2 and f3
    Closes: XUNL-1023

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ function feature1 () {
 }
 
 function feature2 () {
-  console.log("feature 1");
+  console.log("feature 2");
 }
 
 function feature3 () {
-  console.log("feature 1");
+  console.log("feature 3");
 }
 
 (() => {
