e88bd60a6509b68b99b8b0ed0af286d885a94780
vzaiets <jediswear@gmail.com>
2022-01-17 12:58:11 +0200

    feat(XUNL-1021): subject
    body
    
    Refs: https://xunlocked.atlassian.net/browse/XUNL-1206

--- index.js
+++ index.js
@@ -10,8 +10,13 @@ function feature3 () {
   console.log("feature 3");
 }
 
+function feature4 () {
+  console.log("feature 4");
+}
+
 (() => {
   feature1();
   feature2();
   feature3();
+  feature4();
 })();
